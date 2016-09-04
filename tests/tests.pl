3bf9f9ca81702407f3b615a6de3a7328efdab7c0
Aaron Adel <aadel112@gmail.com>
2016-09-04 12:43:09 -0500

    added range tests

--- tests/tests.pl
+++ tests/tests.pl
@@ -1,6 +1,18 @@
 #!/usr/bin/env perl
 
-do '../croon';
+do 'croon';
 use Test::Simple tests => 1;
+use Data::Dumper;
 
-ok( 1, "Stub" );
+test_config();
+
+sub test_config {
+    $CONFIG = 'tests/conf.1';
+    my %cfg = read_cfg();
+    foreach my $k ( keys %cfg ) {
+        #test nice/ionice range adherence
+        ok( $cfg{$k}->{'nice'} <= 20 && $cfg{$k}->{'nice'} >= 0 && $DNICE <= 20 && $DNICE >= 0, "Nice ranges adhered to" );
+        ok( $cfg{$k}->{'ionice'} <= 7 && $cfg{$k}->{'ionice'} >= 0 && $IONICE <= 7 && $IONICE >= 0, "IONice ranges adhered to" );
+
+    }
+}
